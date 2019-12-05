193f50a30b3d371474a05adfb4417d23dd58e8a3
Joel Lacey <joellaceybusiness@gmail.com>
2019-12-04 20:23:39 -0700

    Create pantry check function

--- src/Pantry.js
+++ src/Pantry.js
@@ -3,6 +3,22 @@ class Pantry {
     this.id = id;
     this.pantry = pantry;
   }
+
+  checkPantry(recipe) {
+    return recipe.ingredients.reduce((missingIngredients, recipeIng) => {
+      //check if you cant find the ingredient in the pantry
+      if (!this.pantry.find(pantryIng => pantryIng.ingredient === recipeIng.id)) {
+        //if so add it to acc array
+        missingIngredients.push(recipeIng);
+      }
+      return missingIngredients;
+    }, []);
+    //if this function returns empty array you know you have all the items, otherwise it will return you what you don't have
+  }
+
+  useIngredients() {
+    
+  }
 }
 
 module.exports = Pantry; 
