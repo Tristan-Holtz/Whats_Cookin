ea30f05f0cdae573665b8873453218ca49c3ad2b
Joel Lacey <joellaceybusiness@gmail.com>
2019-12-08 14:25:32 -0700

    Fixed references and added ingredients display

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,3 +1,5 @@
+const ingredientData = require('../data/ingredients');
+
 class Recipe {
   constructor(recipe) {
     this.name = recipe.name;
@@ -6,29 +8,56 @@ class Recipe {
     this.image = recipe.image;
     this.ingredients = recipe.ingredients;
     this.instructions = recipe.instructions;
-  };
+  }
 
   calculateCost() {
     return this.ingredients.reduce((acc, recipeIngredient) => {
-        ingredientData.forEach((ingredient) => {
-          if (ingredient.id === recipeIngredient.id) {
-            acc += ingredient.estimatedCostInCents;
-          }
-        })
+      ingredientData.forEach((ingredient) => {
+        if (ingredient.id === recipeIngredient.id) {
+          acc += ingredient.estimatedCostInCents;
+        }
+      })
       return acc;
     }, 0) / 100;
-  };
+  }
 
   displayInstructions() {
-    return this.instructions.reduce((acc, instruction) => {
+    return JSON.stringify(this.instructions.reduce((acc, instruction) => {
       acc[instruction.number] = instruction.instruction
       return acc;
-    }, {})
-  };
+    }, {}))
+  }
 
-  displayRecipePage() {
-  return `
+  displayIngredients() {
+    return JSON.stringify(this.ingredients.reduce((acc, ingredient) => {
+      acc[ingredient.name] = ingredient.quantity.amount + ' ' + ingredient.quantity.unit; 
+      return acc;
+    }, {}))
+  }
 
+  displayRecipePage(recipe) {
+    return `
+    <section class='recipe'>
+        <section class='recipe__img'></section>
+        <div class='recipe--data'>
+          <div class='recipe--name'>
+            <h1>${recipe.name.toUpperCase()}</h1>
+            <h2>Tags:${recipe.tags}</h2>
+          </div>
+          <button class="recipe__button--favorite"><img id="image" src="../img/favorite.svg" /></button>
+          <button class="recipe__button--cook"><img id="image" src="../img/cook.svg" /></button>
+        </div>
+        <article class='recipe__article--full'>
+          <div class='article--ingredients'>
+            <h2>Ingredients:</h2>
+            <p class='div__p--ingredients'>${recipe.displayIngredients()}</p>
+          </div>
+          <div class='article--instructions'>
+            <h2>Instructions:</h2>
+            <p class='div__p--instructions'>${recipe.displayInstructions()}</p>
+          </div>
+        </article>
+      </section>
   `;
   }
 
