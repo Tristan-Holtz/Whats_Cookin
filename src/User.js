7b9e5dd39c4360bb084953cc2c62847a1ebe76cd
Joel Lacey <joellaceybusiness@gmail.com>
2019-12-08 10:14:20 -0700

    Build out unique favorite and cook recipe handling

--- src/User.js
+++ src/User.js
@@ -5,8 +5,24 @@ class User {
     this.favoriteRecipes = [];
     this.recipesToCook = [];
   }
+
+  storeRecipe(type, recipe) {
+    switch (type) {
+    case 'favorite':
+      this.favoriteRecipes.push(recipe);
+      // this.favoriteRecipes = Array.from(new Set(this.favoriteRecipes))
+      // why JSON stringify?
+      this.favoriteRecipes = this.favoriteRecipes.filter((recipe, index, allRecipes) => index === allRecipes.findIndex((searchIndex) => searchIndex.id === recipe.id))
+      //either way need to reinstanciate as recipes. 
+      break;
+    case 'cook':
+      this.recipesToCook.push(recipe)
+      this.recipesToCook = Array.from(new Set(this.recipesToCook.map(JSON.stringify))).map(JSON.parse)
+      break;
+    }
+  }
 }
 
 if (typeof module !== 'undefined') {
-    module.exports = User;
+  module.exports = User;
 }
