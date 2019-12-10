e8d479a71aa1521b831bcd4a12976b134bb8d9bc
Joel Lacey <joellaceybusiness@gmail.com>
2019-12-10 14:16:08 -0700

    Swap single quotes for double

--- src/Recipe.js
+++ src/Recipe.js
@@ -30,32 +30,32 @@ class Recipe {
 
   displayIngredients() {
     return JSON.stringify(this.ingredients.reduce((acc, ingredient) => {
-      acc[ingredient.name] = ingredient.quantity.amount + ' ' + ingredient.quantity.unit; 
+      acc[ingredient.name] = ingredient.quantity.amount + " " + ingredient.quantity.unit; 
       return acc;
     }, {}))
   }
 
   displayRecipePage() {
     return `
-    <section class='recipe'>
-        <section class='recipe__img'>
-        <img class='article__img' src=${this.image} alt='A picture of ${this.name}'></section>
-        <div class='recipe--data'>
-          <div class='recipe--name'>
+    <section class="recipe">
+        <section class="recipe__img">
+        <img class="article__img" src=${this.image} alt="A picture of ${this.name}"></section>
+        <div class="recipe--data">
+          <div class="recipe--name">
             <h1>${this.name.toUpperCase()}</h1>
             <h2>Tags:${this.tags}</h2>
           </div>
           <button class="recipe__button--favorite"><img id="image" src="../img/favorite.svg" /></button>
           <button class="recipe__button--cook"><img id="image" src="../img/cook.svg" /></button>
         </div>
-        <article class='recipe__article--full'>
-          <div class='article--ingredients'>
+        <article class="recipe__article--full">
+          <div class="article--ingredients">
             <h2>Ingredients:</h2>
-            <p class='div__p--ingredients'>${this.displayIngredients()}</p>
+            <p class="div__p--ingredients">${this.displayIngredients()}</p>
           </div>
-          <div class='article--instructions'>
+          <div class="article--instructions">
             <h2>Instructions:</h2>
-            <p class='div__p--instructions'>${this.displayInstructions()}</p>
+            <p class="div__p--instructions">${this.displayInstructions()}</p>
           </div>
         </article>
       </section>
@@ -64,13 +64,13 @@ class Recipe {
 
   displayRecipeCard(recipe) {
     return `
-      <article data-id='${recipe.id}' class='recipe__article'>
+      <article data-id="${recipe.id}" class="recipe__article">
         <a href="display-recipe.html">
-        <img class='article__img' src=${recipe.image} alt='A picture of ${recipe.name}'></a>
-        <h1 class='article__h1'>${recipe.name}</h1>
-        <div class='article__div'>
-          <button class='article__btn article__btn--favorite'>Favorite</button>
-          <button class='article__btn article__btn--cook'>To Cook</button>
+        <img class="article__img" src=${recipe.image} alt="A picture of ${recipe.name}"></a>
+        <h1 class="article__h1">${recipe.name}</h1>
+        <div class="article__div">
+          <button class="article__btn article__btn--favorite">Favorite</button>
+          <button class="article__btn article__btn--cook">To Cook</button>
         </div>
       </article>
       `;
